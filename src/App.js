c734b7687bc87613b57c56fc14ab427185f79579
Arun Kumar A <arunkumara1367@gmail.com>
2026-02-10 04:20:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx'
+
+function App() {
+  return (
+    <div className="App">
+    <Login/>
+    <Signup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,12 @@
+const Login=()=>{
+    return(
+        <div>
+            <h2>User Login</h2>
+            <input type="email" placeholder="Email"/><br/> <br/>
+            <input type="password" placeholder="Password"/><br/><br/>
+            <button>Login</button>
+        </div>
+        );
+
+    };
+export default Login;
